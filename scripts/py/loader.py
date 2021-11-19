db64b88a571f36f211d5fcc4dc02322b4d2a1d3e
Lanfir7 <50047443+Lanfir7@users.noreply.github.com>
2021-11-19 16:35:00 +0200

    PyVars

--- scripts/py/loader.py
+++ scripts/py/loader.py
@@ -1,10 +1,14 @@
 import os
 import sys
-
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 import importlib
-import threading
+from threading import Thread
 from py4j.java_gateway import JavaGateway, CallbackServerParameters, GatewayParameters
+
+
+class Variables:
+    def __init__(self, value):
+        self.version = value
 
 
 class PBotRunner(object):
@@ -13,12 +17,13 @@ class PBotRunner(object):
         importlib.invalidate_caches()
         script = importlib.import_module(scriptname)
         importlib.reload(script)
-        threading.Thread(target=getattr(script.Script(), methodname), args=[gateway, scriptid]).start()
+        Thread(target=getattr(script.Script(), methodname), args=[gateway, scriptid, gvar]).start()
 
     class Java:
         implements = ["haven.purus.pbot.Py4j.PBotScriptLoader"]
 
 
+gvar = Variables("0.1")
 gateway = JavaGateway(callback_server_parameters=CallbackServerParameters(),
                       python_server_entry_point=PBotRunner,
                       gateway_parameters=GatewayParameters(auto_field=True, auto_convert=True))
--- scripts/py/test.py
+++ scripts/py/test.py
@@ -1,12 +1,39 @@
-class Script:
-    def run(self, gateway, ScriptID):
-        self.gateway = gateway
-        self.ui = gateway.jvm.haven.purus.pbot.PBotAPI.ui()
-        self.pbotutils = gateway.jvm.haven.purus.pbot.PBotUtils
-        window = self.pbotutils.PBotWindow(self.ui, "Butcher", 110, 110, ScriptID)
-        start = window.addButton("skin", "Start", 100, 5, 85)
+class Gateway(object):
+    def __init__(self, gateway):
+        self.__gateway = gateway
 
-    def skin(self, gateway, ScriptID):
+    def ui(self):
+        return self.__gateway.jvm.haven.purus.pbot.PBotAPI.ui()
+
+    def pbotutils(self):
+        return self.__gateway.jvm.haven.purus.pbot.PBotUtils
+
+
+class Script:
+    def run(self, gateway: Gateway, ScriptID, gvar):
+        self.gateway = Gateway(gateway)
+        gvar.skinBol = False
+        gvar.cleanBol = False
+        gvar.butcherBol = False
+        gvar.boneBol = False
+        window = self.gateway.pbotutils().PBotWindow(self.gateway.ui(), "Butcher", 110, 110, ScriptID)
+        skinCheck = window.addCheckbox("skin", "Skin", gvar.skinBol, 5, 5)
+        cleanCheck = window.addCheckbox("btnClick", "Clean", gvar.cleanBol, 5, 25)
+        butcherCheck = window.addCheckbox("btnClick", "Butcher", gvar.butcherBol, 5, 45)
+        boneCheck = window.addCheckbox("btnClick", "Collect bones", gvar.boneBol, 5, 65)
+        start = window.addButton("btnClick", "Start", 100, 5, 85)
+
+
+    def btnClick(self, gateway, ScriptID, gvar):
         self.ui = gateway.jvm.haven.purus.pbot.PBotAPI.ui()
         self.pbotutils = gateway.jvm.haven.purus.pbot.PBotUtils
-        self.pbotutils.sysMsg(self.ui, "HI")
+        self.pbotutils.sysMsg(self.ui, "Skin {}".format(gvar.skinBol))
+
+    def skin(self, gateway, ScriptID, gvar):
+        if not gvar.skinBol:
+            gvar.skinBol = True
+        else:
+            gvar.skinBol = False
+        self.ui = gateway.jvm.haven.purus.pbot.PBotAPI.ui()
+        self.pbotutils = gateway.jvm.haven.purus.pbot.PBotUtils
+        self.pbotutils.sysMsg(self.ui, "Skin {}".format(gvar.skinBol))
